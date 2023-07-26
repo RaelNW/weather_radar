9223faa33ff4dc6974077fbd4de5010a50e184fd
Rael Wanjala <raelnwanjala@gmail.com>
2023-07-26 16:34:04 -0700

    Added search weather function and fetch on js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-fetch("", {
-  cache: "reload",
-})
+
+function searchWeather(){
+  var city = getElementById("cityInput").value;
+
+  var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=f2d2ef0703953b45c83e0ca0964001f7";
+}
+fetch(),
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    displayForecast(data);
   });
