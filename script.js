71e277d68ac0d3df3d4e2c55a7b9838fd12e5390
Rael Wanjala <raelnwanjala@gmail.com>
2023-07-27 02:37:06 -0700

    Added loop function to capture different days of the week, added styling

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function searchWeather() {
     })
     .then(function (data) {
       displayForecast(data);
+      getForecast(city);
     });
 }
 function displayForecast(data) {
@@ -35,4 +36,39 @@ function displayForecast(data) {
   var windSpeedMph = data.wind.speed * 2.237;
   windSpeed.textContent = "Wind Speed: " + windSpeedMph.toFixed(2) + " MPH";
   forecastContainer.appendChild(windSpeed);
+}
+function getForecast(city) {
+  var apiUrl =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&APPID=f2d2ef0703953b45c83e0ca0964001f7";
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var forecastData = data.list.slice(0, 5);
+
+      var forecastHTML = "";
+      for (var i = 0; i < forecastData.length; i++) {
+        var forecastDate = new Date(forecastData[i].dt_txt).toLocaleDateString(
+          "en-US",
+          { weekday: "long", month: "long", day: "numeric" }
+        );
+        var temperature = Math.round(
+          ((forecastData[i].main.temp - 273.15) * 9) / 5 + 32
+        );
+        var windSpeed = Math.round(forecastData[i].wind.speed * 2.237);
+
+        forecastHTML +=
+          "<p>" +
+          forecastDate +
+          ": " +
+          windSpeed +
+          " MPH " +
+          temperature +
+          "°F</p>";
+      }
+      document.getElementById("forecastContainer").innerHTML = forecastHTML;
+    });
 }
